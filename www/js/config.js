0e1eb80d73e9f89e4a3bb580f503295ea4e3cd42
lexvenim <lexvenim@gmail.com>
2016-10-09 21:59:56 +0300

    Added main views and firebase support

--- www/js/config.js
+++ www/js/config.js
@@ -33,7 +33,62 @@ app.config(function ($stateProvider, $urlRouterProvider){
       console.log("settings");
     }
   })
-
+  .state('app.questbook', {
+    url: '/questbook',
+    views: {
+      'menuContent' :{
+        templateUrl: "templates/questbook.html"
+      }
+    },
+    onEnter: function(){
+      console.log("questbook");
+    }
+  })
+  .state('app.skills', {
+    url: '/skills',
+    views: {
+      'menuContent' :{
+        templateUrl: "templates/skills.html"
+      }
+    },
+    onEnter: function(){
+      console.log("skills");
+    }
+  })
+  .state('app.skill', {
+    url: '/skill',
+    views: {
+      'menuContent' :{
+        templateUrl: "templates/skill.html"
+      }
+    },
+    onEnter: function(){
+      console.log("skill");
+    }
+  })
+  .state('app.questline_show', {
+    url: '/questline/show',
+    views: {
+      'menuContent' :{
+        templateUrl: "templates/questline/show.html"
+      }
+    },
+    onEnter: function(){
+      console.log("questline show");
+    }
+  })
+  .state('app.quest_new', {
+    url: '/quest/new',
+    views: {
+      'menuContent' :{
+        templateUrl: "templates/quest/new.html"
+      }
+    },
+    onEnter: function(){
+      console.log("quest new");
+    }
+  })
+  
   $urlRouterProvider.otherwise(function ($injector, $location) {
     var $state = $injector.get("$state");
     $state.go("signin");
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,7 @@
-app.controller('SigninCtrl', function($scope, $state){
+app.controller('SigninCtrl', function($scope, $state, $firebaseArray){
 	$scope.register = function(data){
 		auth.createUserWithEmailAndPassword(data.email, data.password).then(function(result) {
+			saveUser();
 			console.log("User account successfully created.");
 			$state.go('app.profile');
 		}).catch(function(error) {
@@ -23,22 +24,35 @@ app.controller('SigninCtrl', function($scope, $state){
 
 	sign_with_popup = function(provider){
 		auth.signInWithPopup(provider).then(function(result) {
+			var users = $firebaseArray(rootRef.child('users'));
+			users.$loaded().then(function() {
+				if (users.$indexFor(auth.currentUser.uid) == -1) saveUser();
+			})
 			console.log("Authenticated successfully.");
 			$state.go('app.profile');
 		}).catch(function(error) {
 			console.log("Login Failed!", error);
 		});
 	}
+
+	saveUser = function(){
+		var userRef = rootRef.child('users/' + auth.currentUser.uid);
+		userRef.set({
+    	email: auth.currentUser.email,
+  	});
+	}
 });
 
-app.controller('AppCtrl', function($scope, $state){
+app.controller('AppCtrl', function($scope, $state, $firebaseArray, $firebaseObject){
 	$scope.data = {};
 	$scope.providers = {};
+	$scope.data.skills = [];
+	$scope.data.tasks = [];
+	$scope.data.plans = [];
 
 	firebase.auth().onAuthStateChanged(function(user) {
 		if (user) {
-			check_providers();
-			$scope.data.email = auth.currentUser.email;
+			init();
 		} else {
 			$scope.providers = {};
 			$scope.data = {}; 
@@ -69,6 +83,7 @@ app.controller('AppCtrl', function($scope, $state){
 
 	$scope.link_google = function(){
 		link_with_popup(new firebase.auth.GoogleAuthProvider());
+		check_providers();
 	};
 
 	link_with_popup = function(provider){
@@ -85,5 +100,36 @@ app.controller('AppCtrl', function($scope, $state){
 
 	$scope.change_password = function(data){
 		auth.currentUser.updatePassword(data.password);
+	}
+
+	init = function(){
+		check_providers();
+		$scope.data.email = auth.currentUser.email;
+
+		var userData = $firebaseObject(rootRef.child('users/' + auth.currentUser.uid));
+		userData.$loaded().then(function() {
+  		parse_info(userData.plans);
+  		$scope.data.xp = userData.xp;
+  		$scope.data.level = xp_logic(userData.xp);
+  	});
+	}
+
+	parse_info = function(data){
+		for (plan in data) {
+			if ($scope.data.plans.indexOf(plan)==-1) $scope.data.plans.push(plan);
+			for (task in data[plan].tasks) {
+				if ($scope.data.tasks.indexOf(task)==-1) $scope.data.tasks.push(task);
+				var skills = data[plan].tasks[task].skills;
+				for (skill in skills){
+					if ($scope.data.skills.indexOf(skills[skill])==-1) $scope.data.skills.push(skills[skill]);
+				}
+			}
+		}
+	}
+
+	xp_logic = function(data){
+		var level = 1;
+		for (i = 1; data > 50*i*(++i); ) level++;
+		return level;
 	}
 });
